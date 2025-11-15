e1f496b52b2d15a41cdbcac9d330854e96853159
nafila <nafilatk7@gmail.com>
2025-11-15 16:41:15 +0530

    gsap

--- src/app/gsap.from/page.tsx
+++ src/app/gsap.from/page.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+"use client"
+import { gsap } from "gsap"
+import { useEffect, useRef } from "react"
 
-function page() {
-  return (
-    <div>page</div>
-  )
+export default function Home() {
+    const box1Ref = useRef(null)
+    const box2Ref = useRef(null)
+
+
+
+    useEffect(() => {
+
+
+
+        gsap.from(box1Ref.current, {
+            x: -400,
+            y: 0,
+            duration: 5,
+            rotate: 360,
+            scale: 3,
+            opacity: 0.8,
+            ease: "power1.out",
+        }),
+            gsap.from(box2Ref.current, {
+                x: 400,
+                y: 0,
+                duration: 5,
+                rotate: 360,
+                scale: 3,
+                opacity: 0.8,
+                ease: "power1.out",
+            })
+
+    }, [])
+
+
+
+    return (
+        <div>
+            <section>
+
+                <div className="w-screen h-screen flex items-center justify-center bg-black">
+                    <div
+                        ref={box1Ref}
+
+                        className=" w-24 h-24 bg-pink-500  rounded-xl"
+                    >
+                    </div>
+                    <div
+                        ref={box2Ref}
+
+                        className=" w-24 h-24 bg-white  rounded-xl"
+                    >
+                    </div>
+
+                </div>
+            </section>
+
+
+        </div>
+
+
+
+
+
+    )
+
+
 }
-
-export default page
---
+++ src/app/gsap.fromto/page.tsx
@@ -0,0 +1,108 @@
+"use client"
+import { gsap } from "gsap"
+import { useEffect, useRef } from "react"
+
+ export default function Home(){
+    const box1Ref = useRef(null)
+    const box2Ref = useRef(null)
+    const box3Ref = useRef(null)
+    const box4Ref = useRef(null)
+
+
+
+  useEffect(()=>{
+
+    
+
+    gsap.to(box1Ref.current,{
+      x:-400,
+      y:0,
+      duration:5,
+      rotate : 360,
+      scale :3,
+      opacity : 0.8,
+      ease : "power1.out",
+    }),
+    gsap.to(box2Ref.current,{
+      x:400,
+      y:0,
+      duration:5,
+      rotate : 360,
+      scale :3,
+      opacity : 0.8,
+      ease : "power1.out",
+    }),
+    gsap.to(box3Ref.current,{
+      x:1000,
+      y:0,
+      duration:5,
+      rotate : 360,
+      scale :3,
+      opacity : 0.8,
+      ease : "power1.out",
+    }),
+     gsap.to(box4Ref.current,{
+      x:-1000,
+      y:0,
+      duration:5,
+      rotate : 360,
+      scale :3,
+      opacity : 0.8,
+      ease : "power1.out",
+    })
+
+
+  },[])
+
+  
+
+  return (
+    <div>
+      <section>
+
+    <div className="w-screen h-screen flex items-center justify-center bg-black">
+      <div
+        ref={box1Ref}
+      
+        className=" w-24 h-24 bg-pink-500  rounded-xl" 
+      >
+      </div>
+            <div
+        ref={box2Ref}
+      
+        className=" w-24 h-24 bg-white  rounded-xl" 
+      >
+      </div>
+
+    </div>
+      </section>
+    
+
+    <section>
+ 
+    <div className="w-screen h-screen  flex justify-between items-center px-10">
+        <div
+        ref={box3Ref}
+        className=" w-24 h-24 bg-pink-500  rounded-xl" 
+        >
+        </div>
+
+        <div
+        ref={box4Ref}
+        className=" w-24 h-24 bg-white  rounded-xl" 
+        >
+        </div>
+
+    </div>
+
+    </section>
+    </div>
+
+
+    
+
+    
+  )
+
+
+ }
