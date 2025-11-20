f3f0e808a0854cdbe16fae11bd42b5dc50f2a3b0
nafila <nafilatk7@gmail.com>
2025-11-20 12:24:26 +0530

    gsap

--- src/app/scrollTrigger/page.tsx
+++ src/app/scrollTrigger/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -12,41 +13,59 @@ export default function ScrollExample() {
 
   useEffect(() => {
     gsap.to(box3Ref.current, {
+      x: 400,
+      rotate: 360,
+      duration: 3,
+      scrollTrigger: {
+        trigger: box3Ref.current,
+        start: "top center",
+        end: "top 100px",
+        scrub : 1,
+        pin : box3Ref.current,
+        markers: true,
+        toggleActions: "restart pause reverse pause",
+      },
+    });
+    gsap.to(box2Ref.current, {
+      x: 400,
+      rotate: 360,
+      duration: 3,
       scrollTrigger: {
         trigger: box2Ref.current,
-        toggleActions: "restart pause"
+        start: "top center",
+        end: "top 100px",
+        scrub : true,
+        markers: true,
+        toggleActions: "restart pause reverse pause",
       },
-      x: 350,
-      rotate: 360,
-      duration: 3
-    })
-
-
-
-
+    });
   }, []);
 
   return (
-    <div className="flex gap-10 justify-center items-center  m-40 40">
-      <section>
+    <div>
+      <div className="flex justify-center items-center gap-10 m-40">
+        <section className="space-y-24">
 
-        <div
-          ref={box1Ref}
-          className="w-25 h-25 bg-green-400 rounded-xl mx-auto mt-25"
-        />
-        <div
-          ref={box3Ref}
-          className="w-35 h-35 bg-blue-400 rounded-xl mx-auto mt-60"
-        />
-        <div
-          ref={box2Ref}
-          className="w-30 h-30 bg-yellow-400 rounded-xl mx-auto mt-100"
-        />
+          <div
+            ref={box1Ref}
+            className="w-24 h-24 bg-green-400 rounded-xl mx-auto"
+          />
+          <div
+            ref={box2Ref}
+            className="w-28 h-28 bg-yellow-400 rounded-xl mx-auto"
+          />
 
-      </section>
-      <section className="flex gap-10 justify-center items-center  m-40 40">
+          <div
+            ref={box3Ref}
+            className="w-32 h-32 bg-blue-400 rounded-xl mx-auto"
+          />
 
-        <p>
+
+        </section>
+      </div>
+      <div>
+        <section>
+          The standard Lorem Ipsum passage, used since the 1500s
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
           Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
@@ -59,12 +78,9 @@ export default function ScrollExample() {
           "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
 
           1914 translation by H. Rackham
-          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
-
-          help@lipsum.com
-
-        </p>
-      </section>
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness o
+        </section>
+      </div>
 
     </div>
   );
