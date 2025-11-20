6038be9a03b20e70bdd54ab0d47b465a0df2667d
nafila <nafilatk7@gmail.com>
2025-11-20 10:16:26 +0530

    gsap

--- src/app/scrollTrigger/page.tsx
+++ src/app/scrollTrigger/page.tsx
@@ -6,57 +6,39 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function ScrollExample() {
-  const headingRef = useRef(null);
-  const boxRef = useRef(null);
-  const pinSectionRef = useRef(null);
+  const box1Ref = useRef(null);
+  const box2Ref = useRef(null);
+  const box3Ref = useRef(null);
 
   useEffect(() => {
- 
+  gsap.to(box3Ref.current,{
+    x: 350,
+    rotate :360,
+    duration : 2
+  })
 
-    gsap.to(boxRef.current, {
-      rotation: 360,
-      duration: 2,
-      scrollTrigger: {
-        trigger: boxRef.current,
-        start: "top 90%",
-        end: "top 20%",
-        scrub: true,
-      }
-    });
 
-    // Animation 3: Pin a section
-    gsap.from(".pin-text", {
-      x: -200,
-      opacity: 0,
-      duration: 2,
-      scrollTrigger: {
-        trigger: pinSectionRef.current,
-        start: "top top",
-        end: "bottom+=300 top",
-        pin: true,
-        scrub: true,
-      }
-    });
+
 
   }, []);
 
   return (
-    <div className="p-10 space-y-20">
-
-      <h1 ref={headingRef} className="text-4xl font-bold">
-        ScrollTrigger Example
-      </h1>
+    <div className="w-screen h-screen flex gap-10 justify-center items-center bg-black">
+      <section>
 
       <div 
-        ref={boxRef} 
-        className="w-40 h-40 bg-green-500 rounded-xl mx-auto"
+        ref={box1Ref} 
+        className="w-25 h-25 bg-green-400 rounded-xl mx-auto"
+      />
+      <div 
+        ref={box2Ref} 
+        className="w-30 h-30 bg-yellow-400 rounded-xl mx-auto"
+      />
+            <div 
+        ref={box3Ref} 
+        className="w-35 h-35 bg-blue-400 rounded-xl mx-auto"
       />
 
-      <section 
-        ref={pinSectionRef} 
-        className="h-[200vh] bg-gray-800 text-white flex items-center justify-center"
-      >
-        <h2 className="pin-text text-5xl font-bold">Pinned Section</h2>
       </section>
 
     </div>
