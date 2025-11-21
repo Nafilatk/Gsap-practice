2a815032285722e3167c47380c8e1d8da0f2f56e
nafila <nafilatk7@gmail.com>
2025-11-21 16:07:12 +0530

    gsap

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
-      <h2 className="text-2xl font-semibold">MiniStore</h2>
+      <h2 className="text-2xl font-semibold">GSAP</h2>
       <div className="flex gap-6">
         <Link href="/">Home</Link>
         <Link href="/gsap.to">to</Link>
